cef136702b69098132b952daf76d296c1ee716aa
George <gufrar@gmail.com>
2022-02-19 13:37:59 +0300

    нужно больше функций правки для линтера

--- js/main.js
+++ js/main.js
@@ -2,25 +2,21 @@ function getNumber(firstNumber, secondNumber) {
   firstNumber = Math.ceil(firstNumber);
   secondNumber = Math.floor(secondNumber);
   if (firstNumber < 0) {
-    console.log('Введите число от 0 и выше');
-    return;
+    return false;
   }
   if (secondNumber > firstNumber) {
     return Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + firstNumber;
-  };
-  console.log('Второе число должно быть больше чем ' + firstNumber);
-  return;
-};
+  }
+  return false;
+}
 
 getNumber();
 
 function lineLenght(currentLine, maxLine) {
   if (currentLine.length > maxLine) {
-    console.log('Ошибка');
     return false;
   }
-  console.log('Длина строки не превышена');
   return true;
-};
+}
 
 lineLenght();
