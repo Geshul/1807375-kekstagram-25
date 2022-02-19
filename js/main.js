c690fe5f79735a78d207335c4825024ce7d1bd24
George <gufrar@gmail.com>
2022-02-19 13:25:01 +0300

    нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function getNumber(firstNumber, secondNumber) {
+  firstNumber = Math.ceil(firstNumber);
+  secondNumber = Math.floor(secondNumber);
+  if (firstNumber < 0) {
+    console.log('Введите число от 0 и выше');
+    return;
+  }
+  if (secondNumber > firstNumber) {
+    return Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + firstNumber;
+  };
+  console.log('Второе число должно быть больше чем ' + firstNumber);
+  return;
+};
+
+function lineLenght(currentLine, maxLine) {
+  if (currentLine.length > maxLine) {
+    console.log('Ошибка');
+    return false;
+  }
+  console.log('Длина строки не превышена');
+  return true;
+};
