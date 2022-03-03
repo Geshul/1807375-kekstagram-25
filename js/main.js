20e6f8d51cf99d9c18fe9ff53a175adb1effa5e2
George <gufrar@gmail.com>
2022-03-03 21:04:21 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -23,31 +23,33 @@ checkStringMaxLength('Тест', 5);
 
 const DESCRIPTIONS = ['Ну это я','А это не я', 'А это кто?'];
 
-const MESSAGES = ['Всё отлично!',
+const MESSAGES = [
+  'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
   'В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
 const NAMES = ['Борян', 'Стас', 'Линч', 'Дэвид', 'Клод', 'Валерий', 'Виталямбус'];
 
-const RANDOMID = [];
+const randomIds = [];
 
 function getRandomId() {
   let element = getRandomInteger(1,1000);
-  while(RANDOMID.includes(element)){
+  while(randomIds.includes(element)){
     element = getRandomInteger(1,1000);
   }
-  RANDOMID.push(element);
+  randomIds.push(element);
   return element;
 }
 
 function createComments(commentsLength) {
   const comments = [];
   for (let i = 0; i < commentsLength;i++) {
-    comments.push ({
+    comments.push({
       id: getRandomId(),
       avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
       message: MESSAGES[getRandomInteger(0, MESSAGES.length-1)],
