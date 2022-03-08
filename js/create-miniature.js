c57912c42126b3f3aa55c0411d7526a4cb3fbacf
George <gufrar@gmail.com>
2022-03-08 23:01:45 +0300

    отрисуй меня полностью, часть 1

---
+++ js/create-miniature.js
@@ -0,0 +1,19 @@
+import { createPosts } from './create-posts.js';
+const list = document.querySelector('.pictures');
+const templatePost = document.querySelector('#picture')
+  .content;
+
+const posts = createPosts();
+const miniatures = document.createDocumentFragment();
+
+posts.forEach(({url, likes, comments}) => {
+  const post = templatePost.cloneNode(true);
+  post.querySelector('.picture__img').src = url;
+  post.querySelector('.picture__likes').textContent = likes;
+  post.querySelector('.picture__comments').textContent = comments.length;
+  miniatures.append(post);
+});
+
+list.append(miniatures);
+
+export { posts };
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import { createPosts } from './create-posts.js';
+import { posts } from './create-miniature.js';
 
-createPosts();
+createPosts(posts);
